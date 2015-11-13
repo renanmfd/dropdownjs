28177441d186eaacf9a85840900392d74453857f
Renan Dias <renanmfd@gmail.com>
2015-11-13 18:57:16 -0200

    Evolving the plugin

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 (function () {
-    var d = new Dropdown('#page');
-    d.add('.page');
+    var d = new Dropdown(['#page']);
+    d.add('#page');
+    d.add('.js-select');
     d.apply();
     console.log(d);
 }());
