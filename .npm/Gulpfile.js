196816c84ceee47b61de550a3572215691a59aa8
Renan Dias <renanmfd@gmail.com>
2015-11-13 16:48:31 -0200

    Fix gulp config

--- .npm/Gulpfile.js
+++ .npm/Gulpfile.js
@@ -7,24 +7,28 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('gulp-autoprefixer'),
   prettify = require('gulp-html-prettify'),
+  jslint = require('gulp-jslint'),
+  jsmin = require('gulp-jsmin'),
+  rename = require('gulp-rename'),
   reload = browserSync.reload,
   src = {
-    scss: 'scss/**/*.scss',
-    css: 'css',
-    html: '*.html',
+    scss: '../scss/**/*.scss',
+    css: '../css',
+    html: '../app/*.html',
+    js: '../src/*.js',
   };
 
 /**
  * Start the BrowserSync Static Server + Watch files
  */
-gulp.task('serve', ['sass', 'html'], function () {
+gulp.task('default', ['sass', 'js', 'html'] , function () {
 
   browserSync({
-    server: "./app",
-    files: ["css/styles.css", src.html]
+    server: '../app',
   });
 
   gulp.watch(src.scss, ['sass']);
+  gulp.watch(src.js, ['js']);
   gulp.watch(src.html, ['html']);
 });
 
@@ -56,11 +60,31 @@ gulp.task('sass', function () {
   return sassStream()
     .pipe(gulp.dest(src.css))
     .pipe(filter("**/*.css"))
-    .pipe(reload({
-      stream: true
-    }));
+    .pipe(reload({stream: true}));
 });
 
+/**
+ * Verify syntax and minify js.
+ */
+gulp.task('js', function () {
+  gulp.src(src.js)
+    .pipe(jslint({
+      node: true,
+      evil: true,
+      nomen: true,
+      reporter: 'default',
+      edition: '2014-07-08',
+      errorsOnly: false
+    }))
+    .on('error', function (error) {
+        console.error(String(error));
+    })
+    .pipe(jsmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('../app/js/'))
+    .pipe(gulp.dest('../'))
+    .pipe(reload({stream: true}));
+});
 
 /**
  * Generate templates.
@@ -69,5 +93,3 @@ gulp.task('html', function () {
   return gulp.src(src.html)
     .pipe(prettify({indent_char: ' ', indent_size: 2}));
 });
-
-gulp.task('default', ['serve']);
